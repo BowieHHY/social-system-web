c932ec6b253af8f3ae73f96b7b6de096e6ce8fb2
BowieHHY <44061095+BowieHHY@users.noreply.github.com>
2021-09-12 21:30:07 +0800

    fix bug

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
         //前端实现跨域
         proxy:{
             '/api': {
-                target: 'http://localhost:5000/api/',
+                target: 'http://localhost:3000/api/',
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
